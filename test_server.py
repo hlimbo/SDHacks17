005e41ba51c8649e8a8ce1958d62e0e973cbd5bd
fishies <andrewx2@uci.edu>
2017-10-21 01:27:09 -0700

    Basic server for Twilio POSTs

---
+++ test_server.py
@@ -0,0 +1,21 @@
+import http.server
+from auth_key import *
+
+serverAddress, serverPort = ("", 18888)
+
+class TwilioRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        try:
+            self.send_response(204) #no content
+            print(self.requestline)
+            bodyLength = int(self.headers["Content-Length"])
+            requestBody = self.rfile.read(bodyLength).decode("utf-8").split("&")
+            twilioRequest = dict({tuple(i.split("=")) for i in requestBody})
+            print(twilioRequest)
+        except:
+            self.send_error(418) #teapot!
+        finally:
+            pass
+
+daemon = http.server.HTTPServer((serverAddress, serverPort), TwilioRequestHandler)
+daemon.serve_forever()
