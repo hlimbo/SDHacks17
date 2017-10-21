29bf78eeedf0d7c46ed8c6ac1e31746a612569a5
Ubuntu <ubuntu@ip-172-31-31-208.us-west-1.compute.internal>
2017-10-21 23:46:00 +0000

    added join message

--- test_server.py
+++ test_server.py
@@ -1,7 +1,9 @@
 import http.server
 import queue
 import urllib.parse
-import queueProcessor
+#import queueProcessor
+from auth_key import *
+from twilio.rest import Client
 
 serverAddress, serverPort = ("", 18888)
 driftingBottles = queue.Queue()
@@ -13,19 +15,26 @@ pPerSend = 2500
 
 class TwilioRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
+        client = Client(account_sid, auth_token)
+
         try:
             bodyLength = int(self.headers["Content-Length"])
             requestBody = self.rfile.read(bodyLength).decode("utf-8").split("&")
             twilioRequest = dict({tuple(i.split("=")) for i in requestBody})
             twilioRequest["From"] = urllib.parse.unquote_plus(twilioRequest["From"],"utf-8")
             twilioRequest["Body"] = urllib.parse.unquote_plus(twilioRequest["Body"],"utf-8")
-            if twilioRequest["From"] not in users:
-                users[twilioRequest["From"]] = startingP
-            if twilioRequest["Body"].upper() not in keywords:
+            if twilioRequest["Body"] == "JOIN":
+                if twilioRequest["From"] not in users:
+                    users[twilioRequest["From"]] = startingP
+                    client.messages.create(to=users[twilioRequest["From"]], from_=myNumber,body="You just joined!")
+                else:
+                    client.messages.create(to=users[twilioRequest["From"]], from_=myNumber,body="You already joined!")
+            elif twilioRequest["Body"].upper() not in keywords:
                 driftingBottles.put((twilioRequest["From"],twilioRequest["Body"]),block=False)
                 users[twilioRequest["From"]] += pPerSend
             self.send_response(204) #no content
-        except:
+        except Exception as ex:
+            print(ex)
             self.send_error(418) #teapot!
         finally:
             self.end_headers()
@@ -34,4 +43,4 @@ daemon = http.server.HTTPServer((serverAddress, serverPort), TwilioRequestHandle
 daemon.timeout = 1 #in seconds
 while True:
     daemon.handle_request()
-    driftingBottles,users = queueProcessor.processQueue(driftingBottles,users)
+    #driftingBottles,users = queueProcessor.processQueue(driftingBottles,users)
